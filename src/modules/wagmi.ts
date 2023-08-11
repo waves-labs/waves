076723d9d65fa2282a68912812a23fdf044c4666
Afo <obaone01@gmail.com>
2023-08-11 16:15:31 -0700

    got build working had to create custom prod config

--- src/modules/wagmi.ts
+++ src/modules/wagmi.ts
@@ -13,7 +13,35 @@ import { alchemyProvider } from "wagmi/providers/alchemy";
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 
 const { chains, publicClient } = configureChains(
-  [base, baseGoerli, optimism, optimismGoerli, zora, zoraTestnet, foundry],
+  [
+    base,
+    baseGoerli,
+    optimism,
+    optimismGoerli,
+    zora,
+    zoraTestnet,
+    {
+      id: 919,
+      name: "MODE",
+      network: "mode",
+      nativeCurrency: {
+        name: "Ethereum",
+        symbol: "ETH",
+        decimals: 18,
+      },
+      rpcUrls: {
+        default: {
+          http: ["https://sepolia.mode.network"],
+          webSocket: ["wss://sepolia.mode.network"],
+        },
+        public: {
+          http: ["https://sepolia.mode.network"],
+          webSocket: ["wss://sepolia.mode.network"],
+        },
+      },
+    },
+    foundry,
+  ],
   [
     alchemyProvider({ apiKey: import.meta.env.VITE_ALCHEMY_API_KEY! }),
     jsonRpcProvider({
