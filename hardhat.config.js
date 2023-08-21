531432ff3d32031682810c509b873aac620e32aa
Afo <obaone01@gmail.com>
2023-08-21 16:46:32 -0700

    added hardhat packages for proxy contracts

---
+++ hardhat.config.js
@@ -0,0 +1,124 @@
+require("dotenv/config");
+require("hardhat-contract-sizer");
+require("@nomicfoundation/hardhat-foundry");
+require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+
+const {
+  ETHEREUM_OPTIMISM_PROVIDER_URL = "https://mainnet.optimism.io",
+  ETHEREUM_BASE_PROVIDER_URL = "https://mainnet.base.org",
+  ETHEREUM_ZORA_PROVIDER_URL = "https://rpc.zora.energy",
+  ETHEREUM_OPTIMISM_GOERLI_PROVIDER_URL = "https://goerli.optimism.io",
+  ETHEREUM_BASE_GOERLI_PROVIDER_URL = "https://goerli.base.org",
+  ETHEREUM_ZORA_GOERLI_PROVIDER_URL = "https://testnet.rpc.zora.energy",
+  ETHEREUM_MODE_SEPOLIA_PROVIDER_URL = "https://sepolia.mode.network",
+  ETHERSCAN_API_KEY,
+  FORGE_PRIVATE_KEY:
+    deployer = "ledger://0x0000000000000000000000000000000000000000",
+
+  PROFILE: isProfiling,
+} = process.env;
+
+const setDeployer = (address, chain) => ({
+  [chain]: address,
+});
+
+const NamedAccounts = {
+  deployer: {
+    ...setDeployer(deployer, "optimism"),
+    ...setDeployer(deployer, "base"),
+    ...setDeployer(deployer, "zora"),
+    ...setDeployer(deployer, "optimism-goerli"),
+    ...setDeployer(deployer, "base-goerli"),
+    ...setDeployer(deployer, "zora-goerli"),
+    ...setDeployer(deployer, "mode-sepolia"),
+  },
+};
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+    // metadata: {
+    //   bytecodeHash: "none",
+    // },
+    contractSizer: {
+      alphaSort: true,
+      runOnCompile: true,
+      disambiguatePaths: false,
+    },
+    allowUnlimitedContractSize: true,
+    // gasReporter: {
+    //   currency: "USD",
+    //   enabled: isProfiling,
+    // },
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 20,
+        accountsBalance: "10000000000000000000000000000000000000000000000",
+      },
+      allowUnlimitedContractSize: true,
+      saveDeployments: false,
+      live: false,
+    },
+    optimism: {
+      chainId: 10,
+      url: ETHEREUM_OPTIMISM_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+    },
+    base: {
+      chainId: 8453,
+      url: ETHEREUM_BASE_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+    },
+    zora: {
+      chainId: 7777777,
+      url: ETHEREUM_ZORA_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+    },
+    "optimism-goerli": {
+      chainId: 420,
+      url: ETHEREUM_OPTIMISM_GOERLI_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+    },
+    "base-goerli": {
+      chainId: 84531,
+      url: ETHEREUM_BASE_GOERLI_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+      gasPrice: 1000000000,
+      accounts: [deployer],
+    },
+    "zora-goerli": {
+      chainId: 999,
+      url: ETHEREUM_ZORA_GOERLI_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+    },
+    "mode-sepolia": {
+      chainId: 919,
+      url: ETHEREUM_MODE_SEPOLIA_PROVIDER_URL,
+      saveDeployments: true,
+      live: true,
+    },
+  },
+  // paths: {
+  //   deploy: ["contracts/scripts"],
+  // },
+  // namedAccounts: NamedAccounts,
+  verify: {
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+    },
+  },
+};
