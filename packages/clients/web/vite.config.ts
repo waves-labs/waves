19de1f70e43e9f778d5bfc0611911f555e7a76a1
Afo <obaone01@gmail.com>
2023-10-02 17:15:02 -0700

    added assets for waves web remove uneeded assets

--- packages/clients/web/vite.config.ts
+++ packages/clients/web/vite.config.ts
@@ -2,22 +2,117 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
+
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import { VitePWA } from "vite-plugin-pwa";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    mkcert(),
     svgr({
       svgrOptions: {
         namedExport: "RC",
       },
     }),
-    react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
+    VitePWA({
+      includeAssets: [
+        "assets/favicon.ico",
+        "assets/logo.svg",
+        "assets/logo-64.png",
+        "assets/logo-310.png",
+        "assets/house.png",
+        "assets/market.png",
+        "assets/profile.png",
+      ],
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Waves House",
+        short_name: "Waves House",
+        icons: [
+          {
+            src: "assets/logo-64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "assets/logo-310.png",
+            sizes: "192X192",
+            type: "image/png",
+          },
+          {
+            src: "assets/logo-310.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        scope: "/",
+        display: "fullscreen",
+        orientation: "portrait-primary",
+        theme_color: "#e9e3dd",
+        background_color: "#191c1c",
+        shortcuts: [
+          {
+            name: "House",
+            description: "Check what's happening in your house",
+            url: "/house",
+            icons: [
+              {
+                src: "assets/house.png",
+                sizes: "64x64",
+                type: "image/png",
+              },
+            ],
+          },
+          {
+            name: "Market",
+            description: "Cop some new art, apparel, and more",
+            url: "/market",
+            icons: [
+              {
+                src: "assets/market.png",
+                sizes: "64x64",
+                type: "image/png",
+              },
+            ],
+          },
+          {
+            name: "Profile",
+            description: "View accomplishments and stats",
+            url: "/profile",
+            icons: [
+              {
+                src: "assets/profile.png",
+                sizes: "64x64",
+                type: "image/png",
+              },
+            ],
+          },
+        ],
+        related_applications: [
+          {
+            platform: "webapp",
+            url: "https://localhost:5173/manifest.webmanifest",
+          },
+        ],
+        categories: ["entertainment", "music", "social"],
       },
     }),
     nodePolyfills({
